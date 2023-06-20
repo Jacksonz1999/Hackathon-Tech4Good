d72460f6639605ace2c7f58a627626eb72ec5abb
simon hu <simon.hu.1999@gmail.com>
2023-06-20 18:10:46 +0200

    Change file names in the right format

--- explobcn/src/App.jsx
+++ explobcn/src/App.jsx
@@ -1,4 +1,4 @@
-import Login from './Pages/Login/Login.jsx'
+import Login from './pages/login/Login.jsx'
 import './App.css'
 
 function App() {
--- explobcn/src/pages/login/Login.jsx
+++ explobcn/src/pages/login/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './Login.css'
+import './login.css'
 
 function Login() {
   const [email, setEmail] = useState('');
